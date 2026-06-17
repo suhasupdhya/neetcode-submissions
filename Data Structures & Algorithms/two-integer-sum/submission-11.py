5e34fd0b40e2247ccee8f35f85db353862d2cba8
suhasupdhya <supadhya2005@gmail.com>
2026-06-17 15:24:40 +0530

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        suhas={}
+        for i,a in enumerate(nums):
+            diff=target-a
+            if diff in suhas:
+                return [suhas[diff],i]
+            else:
+                suhas[a]=i    
