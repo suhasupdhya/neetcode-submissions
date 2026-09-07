8583979ea50ab75beb5e2109bc8b6d6c1f707015
suhasupdhya <supadhya2005@gmail.com>
2026-09-07 09:11:30 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,6 @@
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count=Counter(nums)
+        return count.most_common(1)[0][0]
+        
